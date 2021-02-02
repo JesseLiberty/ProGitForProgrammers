d418600bc6c7479c9c498ca7df9c54736b3782f8
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-02 08:42:54 -0500

    Add informative WriteLine

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -7,6 +7,7 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
         }
     }
 }
