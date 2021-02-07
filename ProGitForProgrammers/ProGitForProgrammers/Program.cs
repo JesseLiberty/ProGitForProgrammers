c9c89fbedc99ef4feff0b86024a4b158d6055748
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-07 12:08:38 -0500

    This is my commit message

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -9,6 +9,7 @@ namespace ProGitForProgrammers
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this in Visual Studio");
             Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line added to show the staging area");
         }
     }
 }
