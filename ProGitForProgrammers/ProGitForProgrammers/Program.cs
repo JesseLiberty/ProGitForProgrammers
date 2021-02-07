b00ca0967ac8ec4ef732f32ea1b44cb4f67e7e34
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-07 11:53:30 -0500

    Demonstrating the staging area

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -10,6 +10,8 @@ namespace ProGitForProgrammers
             Console.WriteLine("I just added this in Visual Studio");
             Console.WriteLine("I just added this in the command line repo");
             Console.WriteLine("This line added to demonstrate messages.");
+            Console.WriteLine("This line added to show the staging area");
+
         }
     }
 }
