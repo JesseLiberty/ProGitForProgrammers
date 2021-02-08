8434d6d6bdcad6e176da5654a7a379e9f1ed91e8
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-08 12:16:31 -0500

    Add integer division method

--- ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
@@ -20,8 +20,10 @@ namespace ProGitForProgrammers
             return left * right;
         }
 
-
-
+        public int Divide(int left, int right)
+        {
+            return left / right;
+        }
 
     }
 }
