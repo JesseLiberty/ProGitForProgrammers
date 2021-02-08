c3c60c3deebda09633518fa47e40a3b0ba4d0ac8
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-08 09:08:08 -0500

    Add properties

---
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public string Title {get; set;}
+        public List<string> Authors {get; set;}
+
+        public DateTime PublicationDate {get; set;}
+    }
+}
