b03ce53ad5d22eb4746d3ac51b8dd8da94c07e81
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-08 12:15:33 -0500

    Add multiply method

--- ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
@@ -6,9 +6,22 @@ namespace ProGitForProgrammers
 {
     public class Calculator
     {
-        public int Add (int left, int right)
+        public int Add(int left, int right)
         {
             return left + right;
         }
+        public int Subtract(int left, int right)
+        {
+            return left - right;
+        }
+
+        public int Multiply(int left, int right)
+        {
+            return left * right;
+        }
+
+
+
+
     }
 }
